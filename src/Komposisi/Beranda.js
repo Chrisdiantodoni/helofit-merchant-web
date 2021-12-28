9bae787a741498931cec7f6f1b63e61460f17261
Mrfarma <192111237@students.mikroskil.ac.id>
2021-12-28 19:07:28 +0700

    Beranda

---
+++ src/Komposisi/Beranda.js
@@ -0,0 +1,43 @@
+import "../css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Beranda.css";
+import Daftar from "./Daftar";
+import React, { Component } from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import Beranda_2 from "./Beranda (part 2)";
+import ilustrasi from "../Assets/ilustrasi.png";
+export default class Beranda extends Component {
+  render() {
+    return (
+      <div>
+        <Container className='text-center'>
+          <h1 className='pt-5 text-dark'>Creative Digital Agency</h1>
+          <p className='text-muted'>
+            Kami ada untuk membangun strategi dalam meningkatkan penjualan
+            digital usaha Kamu.
+          </p>
+          <Nav.Link href='#porto' className='d-inline-block mx-auto'>
+            <Button variant='primary' className='btn-sm rounded-pill'>
+              Lihat Portofolio
+            </Button>
+          </Nav.Link>
+          <Nav.Link href='/daftar' className='d-inline-block me-5'>
+            <Button
+              variant='light'
+              className='btn-sm btn-outline-primary rounded-pill'>
+              Daftar Gratis
+            </Button>
+          </Nav.Link>
+          <img
+            src={ilustrasi}
+            className='img-fluid d-block mt-5 mx-auto'
+            width='546px'
+            height='500px'
+          />
+          <br />
+          <Beranda_2 />
+        </Container>
+      </div>
+    );
+  }
+}
