d3438fdc00f00417d37fffd2fcdc14702c9a5215
Mrfarma <192111237@students.mikroskil.ac.id>
2022-01-13 00:28:57 +0700

    Tasks(progress)

--- src/Komposisi(User)/Tasks.js
+++ src/Komposisi(User)/Tasks.js
@@ -1,14 +1,44 @@
 import React, { Component } from "react";
+import Textarea from "react-validation/build/textarea";
+import Input from "react-validation/build/input";
+import Form from "react-validation/build/form";
 import AuthService from "../services/auth.service";
 import "../css/bootstrap.min.css";
 import Navbaruser from "../Komponen/Navbar(login user)";
 import Sidebaruser from "../Komponen/Sidebar(login user)";
+import { date } from "yup";
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className='alert alert-danger w-25' role='alert'>
+        Field perlu diisi!
+      </div>
+    );
+  }
+};
+const vfield = (value) => {
+  if (value.length < 3 || value.length > 100) {
+    return (
+      <div className='alert alert-danger w-25' role='alert'>
+        Field harus berisi antara 3 dan 100 karakter.
+      </div>
+    );
+  }
+};
 export class Tasks extends Component {
   constructor(props) {
     super(props);
-
+    var today = new Date(),
+      date =
+        today.getDate() +
+        "/" +
+        (today.getMonth() + 1) +
+        "/" +
+        today.getFullYear();
     this.state = {
       currentUser: AuthService.getCurrentUser(),
+      deadline: "",
+      default: date,
     };
   }
   setValueState(event) {
@@ -16,8 +46,9 @@ export class Tasks extends Component {
       [event.target.name]: event.target.value,
     });
   }
+  componentDidMount() {}
   render() {
-    const { currentUser } = this.state;
+    const { currentUser, deadline, tes } = this.state;
     return (
       <div>
         <Navbaruser konten='To-do List' />
@@ -29,51 +60,50 @@ export class Tasks extends Component {
             <div class='container mx-auto mt-5'>
               <div class='shadow border border-1 rounded-3'>
                 <div className='ms-5 mt-5 me-5 pe-5'>
-                  <table class='table table-borderless'>
-                    <tbody>
-                      <tr className='row'>
-                        <td className='col-2'>
-                          <label className='fw-bold'>Deadline</label>
-                        </td>
-                        <td className='col'>
-                          <input
-                            type='date'
-                            name='awal'
-                            value={this.state.awal}
-                            className='w-25 border border-1'
-                            onChange={this.setValueState.bind(this)}
-                          />
-                          &nbsp;ke&nbsp;
-                          <input
-                            type='date'
-                            name='akhir'
-                            value={this.state.akhir}
-                            className='w-25 border border-1'
-                            onChange={this.setValueState.bind(this)}
-                          />
-                        </td>
-                      </tr>
-                      <tr className='row'>
-                        <td className='col-2'>
-                          <label className='fw-bold'>List</label>
-                        </td>
-                        <td className='col'>
-                          <input
-                            name='List'
-                            value={this.state.list}
-                            className='w-25 border border-1'
-                            onChange={this.setValueState.bind(this)}></input>
-                        </td>
-                      </tr>
-                      <tr className='row'>
-                        <td className='col-2'>
-                          <button className='btn btn-primary rounded rounded-3'>
-                            Tambah List
-                          </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                  <Form>
+                    {tes}
+                    <table class='table table-borderless'>
+                      <tbody>
+                        <tr className='row'>
+                          <td className='col-2'>
+                            <label className='fw-bold'>Deadline</label>
+                          </td>
+                          <td className='col'>
+                            <Input
+                              type='date'
+                              name='deadline'
+                              value={this.state.deadline}
+                              min='2022-01-13'
+                              className='w-25 border border-1'
+                              validations={[required]}
+                              onChange={this.setValueState.bind(this)}
+                            />
+                          </td>
+                        </tr>
+                        <tr className='row'>
+                          <td className='col-2'>
+                            <label className='fw-bold'>List</label>
+                          </td>
+                          <td className='col'>
+                            <Textarea
+                              name='list'
+                              value={this.state.list}
+                              className='w-25 border border-1'
+                              validations={[required, vfield]}
+                              onChange={this.setValueState.bind(this)}
+                            />
+                          </td>
+                        </tr>
+                        <tr className='row'>
+                          <td className='col-2'>
+                            <button className='btn btn-primary rounded rounded-3'>
+                              Tambah List
+                            </button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </Form>
                   <br />
                   <table class='table table-light table-hover'>
                     <tbody>
